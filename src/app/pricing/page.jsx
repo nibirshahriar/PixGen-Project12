eedf612c272181108b02002d9646b64f3ea23689
nibirshahriar <nibir1891@gmail.com>
2026-05-27 04:02:29 +0600

    added proxy

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PricingPage = () => {
+  return (
+    <div className="text-center py-10">
+      Pricing Page is under construction. Please check back later.
+    </div>
+  );
+};
+
+export default PricingPage;
